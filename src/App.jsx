ecd2c4b2e19c5036fbfeaafeab802acaa5e8b1c6
Ipotetico <zbro72@o2.pl>
2023-04-07 00:07:46 +0200

    06-04-23___01

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import Page01 from './components/Page01/Page01';
+import Page02 from './components/Page02/Page02';
+import Page03 from './components/Page03/Page03';
+import Page04 from './components/Page04/Page04';
+import Page05 from './components/Page05/Page05';
+import Page06 from './components/Page06/Page06';
+import Page07 from './components/Page07/Page07';
+import Page08 from './components/Page08/Page08';
+import Page09 from './components/Page09/Page09';
+import Page10 from './components/Page10/Page10';
+import Page11 from './components/Page11/Page11';
+
+const App = () => {
+
+  const [page, setPage] = useState(2);
+
+  return (
+    <>
+      {page === 1 && <Page01 setPage={setPage} />}
+      {page === 2 && <Page02 setPage={setPage} />}
+      {page === 3 && <Page03 setPage={setPage} />}
+      {page === 4 && <Page04 setPage={setPage} />}
+      {page === 5 && <Page05 setPage={setPage} />}
+      {page === 6 && <Page06 setPage={setPage} />}
+      {page === 7 && <Page07 setPage={setPage} />}
+      {page === 8 && <Page08 setPage={setPage} />}
+      {page === 9 && <Page09 setPage={setPage} />}
+      {page === 10 && <Page10 setPage={setPage} />}
+      {page === 11 && <Page11 setPage={setPage} />}
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Page01/Page01.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Page01.scss';
+
+const Page01 = ({ setPage }) => {
+    return (
+        <>
+            <img onClick={() => setPage(8)} src={process.env.PUBLIC_URL + '/images/fwd.svg'} alt="" className="fwd" />
+            <div className="background__container linie">
+                <img src={process.env.PUBLIC_URL + `/images/tlo_linie.webp`} alt="" />
+            </div>
+            <div className="background__container">
+                <img className='okladka' src={process.env.PUBLIC_URL + `/images/zyly_okladka.png`} alt="" />
+            </div>
+
+            <div className="page__container">
+                <header className="header__container">
+                    <div className="icons__container">
+                        <img src={process.env.PUBLIC_URL + `/images/ruszczyk.svg`} alt="" />
+                        <img src={process.env.PUBLIC_URL + `/images/hesperydyna.svg`} alt="" />
+                        <img src={process.env.PUBLIC_URL + `/images/askorbowy.svg`} alt="" />
+                    </div>
+
+                    <h1>STANDARDY LECZENIA <br /> PRZEWLEKŁEJ NIEWYDOLNOŚCI ŻYLNEJ</h1>
+
+                </header>
+                <section className="grades__container">
+                    <img onClick={() => setPage(3)} src={process.env.PUBLIC_URL + `/images/1A.svg`} alt="" />
+                    <img onClick={() => setPage(7)} src={process.env.PUBLIC_URL + `/images/7+.svg`} alt="" />
+                </section>
+            </div>
+            <p className="notes">
+                * EVF - Europejskie Forum Żylne (European Venous Forum) <br />
+                ** ESVS - Europejskie Towarzystwo Chirurgii Naczyniowej (European Society for Vascular Surgery)
+            </p>
+        </>
+    );
+
+};
+
+export default Page01;
